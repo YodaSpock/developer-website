30e624c88904cc053801f26c91c77e6f1fb3d9b5
Isaac Spanier <ispanier@iastate.edu>
2020-03-24 19:58:12 -0500

    added react-router

--- client/src/Pages/ProjectPage.jsx
+++ client/src/Pages/ProjectPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from 'react-router-dom'
 import chess from '../Images/chess.jpg';
 import sequence from '../Images/sequence.jpg';
 import euchre from '../Images/euchre.jpg';
@@ -15,63 +16,67 @@ export default class ProjectPage extends React.Component {
     render() {
         return (
             <div>
-           <Row className = "centerAlign">
-               <Col span = {6}>
-                    <img src = {chess} alt = {"Chess Game"} className = "inprogress" />
-               </Col>
-               <Col span = {6}>
-                    <img src = {sequence} alt = {"Sequence Game"} className = "unfinished" />
-               </Col>
-               <Col span = {6}>
-                    <img src = {euchre} alt = {"Euchre Game"} className = "unfinished"/>
-               </Col>
-               <Col span = {6}>
-                    <img src = {yahtzee} alt = {"Yahtzee Game"} className = "unfinished"/>
-               </Col>
-            </Row>
-            <Row className = "centerAlign" style ={{paddingBottom: "2.5%"}}>
-                <Col span = {6}>
-                    Play Chess
-                </Col>
-                <Col span = {6}>
-                    Play Sequence
-                </Col>
-                <Col span = {6}>
-                    Play Euchre
-                </Col>
-                <Col span = {6}>
-                    Play Yahtzee
-                </Col>
-            </Row>
-            <Row className = "centerAlign">
-               <Col span = {6}>
-                    <img src = {spotify} alt = {"Spotify Projects"} className = "unfinished"/>
-               </Col>
-               <Col span = {6}>
-                    <img src = {instagram} alt = {"Instagram Projects"} className = "unfinished"/>
-               </Col>
-               <Col span = {6}>
-                    <img src = {youtube} alt = {"Youtube Projects"} className = "unfinished"/>
-               </Col>
-               <Col span = {6}>
-                    <img src = {googlemaps} alt = {"Google Maps Projects"} className = "unfinished"/>
-               </Col>
-            </Row>
-            <Row className = "centerAlign" style ={{paddingBottom: "2.5%"}}>
-                <Col span = {6}>
-                    Spotify Projects
-                </Col>
-                <Col span = {6}>
-                    Instagram Projects
-                </Col>
-                <Col span = {6}>
-                    YouTube Projects
-                </Col>
-                <Col span = {6}>
-                    Google Maps Projects
-                </Col>
-            </Row>
-        </div>
+                <Row className = "centerAlign">
+                    <Col span = {6}>
+                         <Link to="/chess">
+                             <img src = {chess} alt = {"Chess Game"} className = "inprogress" />
+                         </Link>
+                    </Col>
+                    <Col span = {6}>
+                        <img src = {sequence} alt = {"Sequence Game"} className = "unfinished" />
+                    </Col>
+                     <Col span = {6}>
+                        <Link to ="/euchre">
+                              <img src = {euchre} alt = {"Euchre Game"} className = "unfinished" />
+                        </Link>
+                    </Col>
+                    <Col span = {6}>
+                        <img src = {yahtzee} alt = {"Yahtzee Game"} className = "unfinished" />
+                    </Col>
+                </Row>
+                <Row className = "centerAlign" style ={{paddingBottom: "2.5%"}}>
+                    <Col span = {6}>
+                        Play Chess
+                    </Col>
+                    <Col span = {6}>
+                        Play Sequence
+                    </Col>
+                    <Col span = {6}>
+                        Play Euchre
+                    </Col>
+                    <Col span = {6}>
+                        Play Yahtzee
+                    </Col>
+                </Row>
+                <Row className = "centerAlign">
+                    <Col span = {6}>
+                        <img src = {spotify} alt = {"Spotify Projects"} className = "unfinished"/>
+                    </Col>
+                    <Col span = {6}>
+                        <img src = {instagram} alt = {"Instagram Projects"} className = "unfinished"/>
+                    </Col>
+                    <Col span = {6}>
+                        <img src = {youtube} alt = {"Youtube Projects"} className = "unfinished"/>
+                    </Col>
+                    <Col span = {6}>
+                        <img src = {googlemaps} alt = {"Google Maps Projects"} className = "unfinished"/>
+                    </Col>
+                </Row>
+                <Row className = "centerAlign" style ={{paddingBottom: "2.5%"}}>
+                    <Col span = {6}>
+                        Spotify Projects
+                    </Col>
+                    <Col span = {6}>
+                        Instagram Projects
+                    </Col>
+                    <Col span = {6}>
+                        YouTube Projects
+                    </Col>
+                    <Col span = {6}>
+                        Google Maps Projects
+                    </Col>
+                </Row>
+            </div>
         )
         //MINESWEEPER
         //BLACKJACK
