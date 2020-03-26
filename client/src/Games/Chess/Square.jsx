7c06e776f5594acd87854397c2685608021109e3
Isaac Spanier <ispanier@iastate.edu>
2020-03-26 11:45:56 -0500

    new folder structure for chess pieces

--- client/src/Games/Chess/Square.jsx
+++ client/src/Games/Chess/Square.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
 import "./Square.scss"
-import WhiteRook from './ChessPieces/WhiteRook.png';
-import BlackRook from './ChessPieces/BlackRook.png';
-
 export default class Square extends React.Component{
     state = {}
 
@@ -12,20 +9,20 @@ export default class Square extends React.Component{
     render(){
         const{
             isBlack,
-            //column,
+            occupied,
+            piece
             //row
         } = this.props
 
         let squareName = "square";
         if(isBlack === 'true'){
-            console.log("here");
             squareName = "square-black"
         }
 
         return(
             <div className ={squareName}>
                 <div className = "content">
-                    <img src = {BlackRook} alt = {"White Rook"} className = "chessPiece" />
+                    {occupied ? piece : null}
                 </div>
             </div>
         )
