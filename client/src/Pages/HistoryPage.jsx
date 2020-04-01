49be221574cdd14b9939132a7d33633b900775f7
Isaac Spanier <ispanier@iastate.edu>
2020-04-01 00:13:56 -0500

    senior deisgn write up

--- client/src/Pages/HistoryPage.jsx
+++ client/src/Pages/HistoryPage.jsx
@@ -36,7 +36,7 @@ export default class HistoryPage extends React.Component {
                     The portfolio is made using React.js front end with help from ant design's react ui and sass styling. The backend for 
                     my projects such as chess is using node.js. I am currently working on developing the chess game to be played against 
                     an AI chess bot that will be written in python. I am currently enrolled in a machine learning course at Iowa State, 
-                    ComS 474 and I hope to put some of my knowledge to use in this project. 
+                    ComS 474, and I hope to put some of my knowledge to use in this project. 
                   </p>
                 </TimelineItem>
                 <TimelineItem
@@ -54,18 +54,14 @@ export default class HistoryPage extends React.Component {
                   <h3>Senior Design Project</h3>
                   <h4>Intelligent Code Editor - Testing Lead</h4>
                   <p>
-                    Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
-                    exercitation. Veniam velit adipisicing anim excepteur nostrud magna
-                    nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
-                    reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis Lorem
-                    est.
-                  </p>
-                  <p>
-                    Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
-                    exercitation. Veniam velit adipisicing anim excepteur nostrud magna
-                    nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
-                    reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis Lorem
-                    est.
+                    During the course of senior year, myself and 5 others have been slowly but surely developing an Intelligent Code
+                    Editor. The project is to take natural language and convert that into executable Java code. We made an IntelliJ 
+                    plugin that allows users to highlight natural language and translate into Java code. We do this by running a machine
+                    learning OpenNMT-py model through with an extensive hand-made dataset that allows users to select from multiple options
+                    when doing translations. We do a fair amount of preprocessing with the data, something I particularly worked on, by 
+                    filtering out words that aren't nouns or verbs. I also worked on creating a large dataset for our model to use, since 
+                    we unable to find a suitable dataset for our purposes. As the semester comes to a close I will be sad to part with this
+                    project.
                   </p>
                 </TimelineItem>
                 <TimelineItem
