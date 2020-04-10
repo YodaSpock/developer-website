cadd090ad09754314c0bfdc5e6b92af5a4223d31
Isaac Spanier <ispanier@iastate.edu>
2020-04-10 18:45:44 -0500

    added about and skills

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {GithubFilled, LinkedinFilled, YoutubeFilled} from '@ant-design/icons';
 import profile from '../Images/profile.jpg';
 import "../Styles/Background.scss";
+import "../Styles/Home.scss"
 import {Row, Col, Divider} from 'antd';
 import { Typography } from 'antd';
 
@@ -21,28 +22,32 @@ export default class HomePage extends React.Component {
                 <Col span = {4}/>
             </Row>
             <Row>
+                <Col span = {6}/>
+                <Col span = {12}>
+                    <Title style = {{textAlign: "center", color: "#0C2340", fontSize: "30px"}}>Full Stack Developer</Title>
+                </Col>
+                <Col span = {6}/>
+            </Row>
+            <Row>
                 <Col span = {10}/>
                 <Col span = {4}>
                     <img src = {profile} className ="profile" alt = {"profile"} style = {{height: "100%", borderRadius: "100%", width: "100%"}}/><br/>
                 </Col>
                 <Col span = {10}/>
             </Row>
+            
             <Row>
                 <Col span = {8}/>
                 <Col span = {8} style = {{textAlign: "center", fontSize: "50px"}}>
-                    <a href="https://www.linkedin.com/in/isaacspanier/">
+                    <a style = {{padding: "1%"}} href="https://www.linkedin.com/in/isaacspanier/">
                         <LinkedinFilled style = {{color: "blue",}}/>    
                     </a>
-                    <a href = "https://github.com/YodaSpock">
+                    <a style = {{padding: "1%"}} href = "https://github.com/YodaSpock">
                         <GithubFilled style = {{color: "black",}}/> 
                     </a>
-                    <a href = "https://www.youtube.com/channel/UCIzt2QpLmaFUMDXETm_qWkQ" >
+                    <a style = {{padding: "1%"}} href = "https://www.youtube.com/channel/UCIzt2QpLmaFUMDXETm_qWkQ" >
                         <YoutubeFilled style = {{color: "red", }}/>
                     </a>
-                
-                
-                
-                    
                 </Col>
                 <Col span = {8}/>
             </Row>
@@ -66,33 +71,9 @@ export default class HomePage extends React.Component {
                 <Col span = {2}/>
             </Row>
             <Row>
-                <Col span = {24}>
-                    <Divider style = {{backgroundColor: "black", height: "2.5px"}}/>
-                </Col>
-            </Row>
-            <Row>
                 <Col span = {8}/>
                 <Col span = {8}>
-                    <Title  style= {{textAlign: "center"}}>Work Experience</Title>
-                </Col>
-                <Col span = {8}/>
-            </Row>
-            <Row>
-                <Col span = {2}/>
-                <Col span = {20}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </Col>
-                <Col span = {2}/>
-            </Row>
-            <Row>
-                <Col span = {24}>
-                    <Divider style = {{backgroundColor: "black", height: "2.5px"}}/>
-                </Col>
-            </Row>
-            <Row>
-                <Col span = {8}/>
-                <Col span = {8}>
-                    <Title  style= {{textAlign: "center"}}>Skills</Title>
+                    <Title style= {{textAlign: "center"}}>Skills</Title>
                 </Col>
                 <Col span = {8}/>
             </Row>
