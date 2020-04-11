64fc37339dc4febdd4c084ac2f28811af43256ef
Isaac Spanier <ispanier@iastate.edu>
2020-04-10 20:27:42 -0500

    other page populated'

--- client/src/Pages/OtherPage.jsx
+++ client/src/Pages/OtherPage.jsx
@@ -1,16 +1,73 @@
 import React from "react";
+import {Divider, Typography, Row, Col} from "antd";
 import "../Styles/Background.scss";
+const { Title } = Typography;
 
 export default class OtherPage extends React.Component {
 
     render() {
         return (
-           <div className = "backgroundOther">
-               STAR WARS <br/>
-               BILLIARDS <br/>
-               CHOIR <br/>
-               OTHER STUFF <br/>
-           </div>
+            <Row className = "backgroundOther">
+                <Col span = {24}>
+                    <Title style ={{textAlign: "center"}}>STAR WARS</Title><br/>
+                    <Row>
+                        <Col span = {6}>
+                            <img/>
+                        </Col>
+                        <Col span = {18}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </Col>
+                    </Row>
+                    <Divider style = {{backgroundColor: "black"}} />
+                </Col>
+                <Col span = {24}>
+                    <Title style ={{textAlign: "center"}}>BILLIARDS</Title> <br/>
+                    <Row>
+                        <Col span = {6}>
+                        
+                        </Col>
+                        <Col span = {18}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </Col>
+                    </Row>
+                    <Divider style = {{backgroundColor: "black"}} />
+                </Col>
+                <Col span = {24}>
+                    <Title style ={{textAlign: "center"}}>CHOIR</Title> <br/>
+                    <Row>
+                        <Col span = {6}>
+                        
+                        </Col>
+                        <Col span = {18}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </Col>
+                    </Row>
+                    <Divider style = {{backgroundColor: "black"}} />
+                </Col>
+                <Col span = {24}>
+                    <Title style ={{textAlign: "center"}}>BOARD GAMES</Title><br/>
+                    <Row>
+                        <Col span = {6}>
+                        
+                        </Col>
+                        <Col span = {18}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
         )
     }
 
