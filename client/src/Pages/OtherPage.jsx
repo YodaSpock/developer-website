c5c31e03865efef681c3cb3094daee7930caa060
Isaac Spanier <ispanier@iastate.edu>
2020-04-15 01:42:35 -0500

    fixed other page sizing

--- client/src/Pages/OtherPage.jsx
+++ client/src/Pages/OtherPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Divider, Typography, Row, Col} from "antd";
-import starWars from '../Images/StarWarsAdam.jpg';
+import starWars from '../Images/StarWarsAdam2.jpg';
 import billiards from '../Images/billiardsclub.jpg';
 import choir from '../Images/CarnegieHall.jpg';
 import games from '../Images/BoardGames.jpg';
@@ -15,62 +15,62 @@ export default class OtherPage extends React.Component {
         return (
             <div className = "backgroundOther">
                     <Row>
-                        <Col span = {10}>
+                        <Col span = {6}>
                             <img src = {starWars} alt = {"StarWars"} className = "starwars" />
                         </Col>
-                        <Col span = {14}>
-                        <Title style ={{textAlign: "left"}}>STAR WARS</Title><br/>
-                        <div className = "text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
-                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </div>
+                        <Col span = {18}>
+                            <Title style ={{textAlign: "left"}}>STAR WARS</Title><br/>
+                            <div className = "text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </div>
                         </Col>
                     </Row>
                     <Divider style = {{backgroundColor: "black"}} />
                     <Row>
-                        <Col span = {10}>
+                        <Col span = {6}>
                             <img src = {billiards} alt = {"Billiards"} className = "starwars" />
                         </Col>
-                        <Col span = {14}>
-                        <Title style ={{textAlign: "left"}}>BILLIARDS</Title> <br/>
-                        <div className = "text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
-                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </div>
+                        <Col span = {18}>
+                            <Title style ={{textAlign: "left"}}>BILLIARDS</Title> <br/>
+                            <div className = "text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </div>
                         </Col>
                     </Row>
                     <Divider style = {{backgroundColor: "black"}} />
                     <Row>
-                        <Col span = {10}>
+                        <Col span = {6}>
                             <img src = {choir} alt = {"Choir"} className = "starwars" />
                         </Col>
-                        <Col span = {14}>
-                        <Title style ={{textAlign: "left"}}>CHOIR</Title> <br/>
-                        <div className = "text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
-                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </div>
+                        <Col span = {18}>
+                            <Title style ={{textAlign: "left"}}>CHOIR</Title> <br/>
+                            <div className = "text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </div>
                         </Col>
                     </Row>
                     <Divider style = {{backgroundColor: "black"}} />
                     <Row>
-                        <Col span = {10}>
+                        <Col span = {6}>
                             <img src = {games} alt = {"StarWars"} className = "starwars" />
                         </Col>
-                        <Col span = {14}>
-                        <Title style ={{textAlign: "left"}}>BOARD GAMES</Title><br/>
-                        <div className = "text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
-                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </div>
+                        <Col span = {18}>
+                            <Title style ={{textAlign: "left"}}>BOARD GAMES</Title><br/>
+                            <div className = "text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </div>
                         </Col>
                     </Row>
             </div>
