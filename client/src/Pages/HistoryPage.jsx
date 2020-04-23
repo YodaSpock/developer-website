c8bee38c185177a6a44c119b10bd310758e72435
Isaac Spanier <ispanier@iastate.edu>
2020-04-22 23:47:47 -0500

    added some small changes

--- client/src/Pages/HistoryPage.jsx
+++ client/src/Pages/HistoryPage.jsx
@@ -25,10 +25,10 @@ export default class HistoryPage extends React.Component {
                   <h3>Personal Website</h3>
                   <h4>Isaac Spanier Portfolio - Lead Developer {emoji('😎')}</h4>
                   <p className = "historyParagraph">
-                   This project was started on March 21st and is what you currently going through. It was a way for me to show
-                   off some things that I can do, and to work on some fun projects duing the COVID-19 pandemic. It's goal is to 
-                   help me get a full time job for starting after my May 2020 graduation. And yes I am the lead and only developer 
-                   on this project. 
+                    This project was started on March 21st and is what you currently going through. It was a way for me to show
+                    off some things that I can do, and to work on some fun projects duing the COVID-19 pandemic. It's goal is to 
+                    help me get a full time job for starting after my May 2020 graduation. And yes I am the lead and only developer 
+                    on this project. 
                   </p>
                   <p className = "historyParagraph">
                     The portfolio is made using React.js front end with help from ant design's react ui and sass styling. The backend for 
@@ -122,23 +122,23 @@ export default class HistoryPage extends React.Component {
                   <h3>Buildertrend Internship</h3>
                   <h4>Worked as Software Development Intern</h4>
                   <p className = "historyParagraph">
-                   I recently had the privelege of landing an summer intership at Buildertrend, a leading software development company based in Omaha,
-                   Nebraska, that focuses on building construction managment 
-                   software for helping builders and contractors run their business. There have been ranked as the #1 place to work in Omaha, 
-                   Nebraska for the past two years, and I can say it was an amazing place to work.
+                    I recently had the privelege of landing an summer intership at Buildertrend, a leading software development company based in Omaha,
+                    Nebraska, that focuses on building construction managment 
+                    software for helping builders and contractors run their business. There have been ranked as the #1 place to work in Omaha, 
+                    Nebraska for the past two years, and I can say it was an amazing place to work.
                   </p>
                   <p className = "historyParagraph">
-                   Over my time there, I became very familiar with React devlopment as my job became converting old web forms pages into new React 
-                   components. This greatly improved the loading time for the web application, and also improved the look of the website. I also 
-                   learned and improved my skills in  .NET framework, Typescript, Javascript, MySQL, CSS, HTML, and learned 
-                   how to work in an Agile style environment. At Buildertrend we worked in teams of 5 developers and we incorporated daily standups 
-                   and peer code reviews into our routine. 
+                    Over my time there, I became very familiar with React devlopment as my job became converting old web forms pages into new React 
+                    components. This greatly improved the loading time for the web application, and also improved the look of the website. I also 
+                    learned and improved my skills in  .NET framework, Typescript, Javascript, MySQL, CSS, HTML, and learned 
+                    how to work in an Agile style environment. At Buildertrend we worked in teams of 5 developers and we incorporated daily standups 
+                    and peer code reviews into our routine. 
                   </p>
                   <p className = "historyParagraph">
-                  During the internship I competed with 3 fellow interns in a hackathon housted by Buildertrend, and we tried to develop a crude version
-                  of an improved image editor. We spent 48 developing and had to present our final product at the end of the weekend. We ended up winning 
-                  people's choice for the event. At the end of the intership I had finished several components and I am very proud to say that all the 
-                  components I developed are currently in production and are helping contractors and builders improve their business. 
+                    During the internship I competed with 3 fellow interns in a hackathon housted by Buildertrend, and we tried to develop a crude version
+                    of an improved image editor. We spent 48 developing and had to present our final product at the end of the weekend. We ended up winning 
+                    people's choice for the event. At the end of the intership I had finished several components and I am very proud to say that all the 
+                    components I developed are currently in production and are helping contractors and builders improve their business. 
                   </p>
                 </TimelineItem>
                 <TimelineItem
